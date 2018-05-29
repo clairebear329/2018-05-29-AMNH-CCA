1dd87a7b5268f8fcae66342470230fff2b19e0aa
AstroCom <astrocom@Davids-MacBook-Pro.local>
2018-05-29 15:18:41 -0400

    deleted extra nonsense

--- Day1Bash/example_loop.sh
+++ Day1Bash/example_loop.sh
@@ -1,11 +1,3 @@
-# I am creating a variable greeting and setting it to hello world
-greeting='Hello World'
-
-#I am printing the contents of 'greeting'
-echo $greeting
-
-#Touch and print a range of files in a loop
-
 for myfile in {A..Z}.txt;
 do
     touch $myfile
