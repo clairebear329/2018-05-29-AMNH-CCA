528e61930d6ea6492e608beb00b9ae015c704384
AstroCom <astrocom@Davids-MacBook-Pro-2.local>
2018-05-30 15:25:52 -0400

    simple Python functions added

---
+++ Day2Python/functions_to_use.py
@@ -0,0 +1,9 @@
+#Accessing tools (methods and attributes) from the functions_to_use module
+'''This module contains some functions we will create.'''
+
+def division_function_to_test(numerator, denominator) :
+    
+    return float(numerator)/denominator
+
+#Example attribute
+example_denominator=300
